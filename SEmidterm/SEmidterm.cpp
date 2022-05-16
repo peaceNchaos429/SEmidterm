8760ec8da92cb3519f50e4f7614d479d8e3fc158
Briana Buttone <BKButtone@student.fullsail.edu>
2022-05-16 13:58:29 -0400

    Add project files.

---
+++ SEmidterm/SEmidterm.cpp
@@ -0,0 +1,147 @@
+// SEmidterm.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int response;
+	std::cout << "What is the product of 2 * 6? \n" << "1) 13 \n" << "2) 28 \n" << "3) 12 \n" << "4) 45 \n" << std::endl;
+	std::cin >> response;
+	switch (response)
+	{
+	case 1:
+
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+
+	case 2:
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+
+	case 3:
+
+
+		std::cout << "correct \n" << std::endl;
+		break;
+
+	case 4:
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	}
+	std::cout << "Who was the first president? \n" << "1) Tommy Lee Jones \n" << "2) George Washington \n" << "3) Barrak Obama \n" << "4) Johnny Depp \n" << std::endl;
+	std::cin >> response;
+	switch (response)
+	{
+	case 1:
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	case 2:
+
+
+		std::cout << "correct \n" << std::endl;
+		break;
+
+	case 3:
+
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	case 4:
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	}
+	std::cout << "How many states are in the United States? \n" << "1) 50 \n" << "2) 25 \n" << "3) 47 \n" << "4) 100 \n" << std::endl;
+	std::cin >> response;
+
+	switch (response)
+	{
+	case 1:
+
+
+		std::cout << "correct \n" << std::endl;
+		break;
+
+	case 2:
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	case 3:
+
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	case 4:
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	}
+
+	std::cout << "What is the sqaure root of 36? \n" << "1) 22 \n" << "2) 46 \n" << "3) 2 \n" << "4) 6 \n" << std::endl;
+	std::cin >> response;
+
+	switch (response)
+	{
+	case 1:
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	case 2:
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	case 3:
+
+
+
+		std::cout << "sorry that answer is incorrect \n" << std::endl;
+		break;
+
+	case 4:
+
+
+		std::cout << "correct \n" << std::endl;
+		break;
+
+	}
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
